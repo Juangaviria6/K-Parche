83b0bd1521e138398ec2cca93b6558527a674df3
Juangaviria6 <164352639+Juangaviria6@users.noreply.github.com>
2026-03-28 10:52:56 -0500

    Arreglo Archivo .env

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/aut
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCI5FYXxHbyuBUkmwSHdVD_8uwjfh5apIc",
-  authDomain: "k-parche.firebaseapp.com",
-  projectId: "k-parche",
-  storageBucket: "k-parche.firebasestorage.app",
-  messagingSenderId: "115578472603",
-  appId: "1:115578472603:web:4087a34654a77886d3de3e"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
